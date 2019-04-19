3dd3c3c58c486ea67d519a67047d80e3ce3bfd0c
Peyton Breech <peyton.breech16@gmail.com>
2019-04-19 11:32:07 -0500

    Test

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger))]
+public class CubeBehavior : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.AddListener(EventTriggerType.PointerDown, Hold);
+        gameObject.AddListener(EventTriggerType.PointerUp, Release);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Hold()
+    {
+        Transform pointerTransform = GvrPointerInputModule.Pointer.PointerTransform;
+
+        transform.SetParent(pointerTransform, true);
+    }
+
+    public void Release()
+    {
+        transform.SetParent(null, true);
+    }
+}
+
+public static class EventExtensions
+{
+
+    public static void AddListener(this GameObject gameObject, EventTriggerType eventTriggerType, UnityAction action)
+    {
+        EventTrigger eventTrigger = gameObject.GetComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = eventTriggerType;
+        entry.callback.AddListener(_ => action());
+
+        eventTrigger.triggers.Add(entry);
+    }
+
+}
