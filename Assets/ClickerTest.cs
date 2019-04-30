b1db08d5f9224166f07ef8d717aefe9a0735fa56
Peyton Breech <peyton.breech16@gmail.com>
2019-04-30 18:43:27 -0500

    Working Star Managers

---
+++ Assets/ClickerTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickerTest : MonoBehaviour
+{
+    public bool clicked;
+
+    public void Update()
+    {
+        clicked = true;
+    }
+}
--- Assets/Scripts/StarManager.cs
+++ Assets/Scripts/StarManager.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class StarManager : MonoBehaviour
 {
     public int counter = 0;
-    public bool star1 = false;
-    public bool star2 = false;
     public GameObject[] stars = new GameObject [20];
     public GameObject[] lines = new GameObject [19];
-    public GameObject test;
+    public int max;
 
     public void Update()
     {
-        
-
-        if(stars[counter].isStatic && stars[counter+1].isStatic)
+        if (stars[counter].isStatic && stars[counter + 1].isStatic)
         {
             lines[counter].SetActive(true);
+            stars[counter+1].SetActive(false);
             counter++;
-            test.tag.Equals("Done");
+        }
+
+        for (int check = counter+2; check < stars.Length-1; check++)
+        {
+            if (stars[counter].isStatic && stars[check].isStatic && !stars[counter+1].isStatic)
+            {
+                //play wrong noise
+                stars[check].isStatic = false;
+            }
+        }
+
+        if (stars[max].isStatic)
+        {
+            //load ui informational screen with next level button
         }
     }
 }
