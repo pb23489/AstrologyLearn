40c123f63014db1f8c5ea7f5e9f6a77b1f3fde01
Peyton Breech <peyton.breech16@gmail.com>
2019-05-08 12:04:11 -0500

    Working ending UI

--- Assets/Scripts/StarManager.cs
+++ Assets/Scripts/StarManager.cs
@@ -10,13 +10,19 @@ public class StarManager : MonoBehaviour
     private int max;
     public GameObject[] stars = new GameObject [0];
     public GameObject[] lines = new GameObject [0];
+    public GameObject endScreen;
+    public GameObject continueButton;
 
     public void Update()
     {
        max = stars.Length-1;
         if (counter == max)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            endScreen.SetActive(true);
+            if (continueButton.CompareTag("Clicked"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
 
         else if (stars[counter].CompareTag("Clicked") && stars[counter + 1].CompareTag("Clicked"))
